cdf3195b59b43aedbc4bcf1fcfdefef5d3be075c
yanixn <2896399920@qq.com>
2023-03-31 13:29:19 +0800

    后台搭建

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+    <Header></Header>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+
+</style>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: 'APP',
+  components: {
+
+    Header
+
+  }
+}
+</script>
